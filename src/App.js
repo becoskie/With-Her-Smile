ff838b2908d0a7ed1d50091165441464489e16cc
Ray Becoskie <ray@becoskie.com>
2018-09-20 10:37:22 -0500

    added Mary component with images and json file, tried removing flavicon, try later

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import Wrapper from "./components/Wrapper";
+import Navbar from "./components/NavBar";
+import MaryCard from "./components/MaryCard";
+import mary from "./mary.json"
+import "./App.css";
 
 class App extends Component {
+  state = {
+    mary,
+    clickedMary: [],
+    score: 0
+  };
+  
   render() {
     return (
-
+      <div>
+        <Navbar />
+         <Wrapper>
+         {this.state.mary.map(mary => (
+            <MaryCard
+              imageClick={this.imageClick}
+              id={mary.id}
+              key={mary.id}
+              image={mary.image}
+            />
+          ))}
+        </Wrapper>
+      </div>
     );
   }
 }
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,27 @@
+//sets up the reusable Navbar component
+import React, { Component } from "react";
+import "./NavBar.css";
+
+class Navbar extends Component {
+  render() {
+    return (
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Help Mary Make It!</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
