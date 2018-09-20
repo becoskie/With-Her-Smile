490987899be56c87faf0323bcde2fae25f5d3324
Ray Becoskie <ray@becoskie.com>
2018-09-20 11:24:39 -0500

    Added image styling and hover effects

--- src/components/MaryCard/MaryCard.js
+++ src/components/MaryCard/MaryCard.js
@@ -4,8 +4,8 @@ import "./MaryCard.css";
 
 //pass the image into each card so all 12 are rendered
 const FriendCard = props => (
-  <div MaryCard="card" onClick={props.imageClick}>
-    <div className="img-container">
+  <div className="card" onClick={props.imageClick}>
+    <div className="image-container">
       <img alt={props.image.replace(".jpg", "")} src={require("../../images/" + props.image)} />
     </div>
   </div>
