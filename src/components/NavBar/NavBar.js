bd9b8decae27751f389603a1d0998998fc3aa48f
Ray Becoskie <ray@becoskie.com>
2018-09-20 14:09:35 -0500

    style nav like example

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,20 +5,12 @@ import "./NavBar.css";
 class Navbar extends Component {
   render() {
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Help Mary Make It!</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-      
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            
+        <nav className="navbar">
+          <ul>
+          <li>Help Mary Make It!</li>
+            <li>Your Guess</li>
+            <li>Score: 0 | Top Score: 12</li>
           </ul>
-        </div>
       </nav>
     );
   }
