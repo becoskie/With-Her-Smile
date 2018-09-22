a955e3c8f3be07b75cac3825414a4e61346ae273
Ray Becoskie <ray@becoskie.com>
2018-09-22 16:40:09 -0500

    more wordy changes

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -8,12 +8,12 @@ const Home = props => (
     <div className="col-md-4">
     <h2>Keeping Mary Fresh</h2>
     <p>
-      If She's going to "make it", we need to keep our girl fancy fresh.</p>
+      If she's going to "make it" we need to make our girl fancy fresh.</p>
       <p>
       She has 12 outfits.
     </p>
     <p>
-    So keep her in a new one for 12 days straight to keep her <br/><span id="fab">FABULOUS!</span>
+    Choose a new outfit for 12 days straight to keep her <br/><span id="fab">FABULOUS!</span>
     </p>
     <button
       type="button"
